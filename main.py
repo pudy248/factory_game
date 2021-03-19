f9e1c26a1bc9081f4d57ef44c5c4afe9f9ec076f
Menvolk <somerandenguy@yahoo.com>
2021-03-19 13:04:56 -0600

    This is lame

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-Iamafluffybunny = 2
-IamafluffybunnyIamafluffybunny = Iamafluffybunny
-assert Iamafluffybunny == IamafluffybunnyIamafluffybunny, "Don't talk to me or my son ever again."
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+test_change = True
+Ries = "Ries"
+Mean = 0
+assert Ries == Mean, "Ries please stop making fun of me I'm a useless cow"
