3724dda049aabbee47ccb31b229d833dea4c51ea
mybarlucea <21michael.ybarlucea@boiseschools.net>
2021-03-30 13:06:30 -0600

    text parsing for loading level

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 SURF = pg.display.set_mode((W, H), pg.NOFRAME)
 
+
 #####CONSTANTS#####
 FPS = 60
 TILE_SIZE = 50  # dimensions of each tile in pixels
@@ -101,10 +102,40 @@ class Manufacturer(Tile):
         # if Recipie Collection class says that the items in self.items can be made into a recipie, consumes them and outputs the result
         super(Manufacturer, self).tick()
 
+class Level:
+    def __init__(self, map):
+        self.map = map
+
+    def world_tick(self):
+        x = 4
+
+    def draw_level(self):
+        print(self.map)
+
+class Loader:
+    def __init__(self):
+        self.lNum = 0
+
+    def load_level(self, n):
+        self.lNum = n
+        text = str(n) + '.txt'
+        lines = []
+        map = []
+        with open(text, 'rt') as myfile:
+            for line in myfile:
+                lines.append(line.rstrip(" \n"))
+        for i in range(len(lines)):
+            map.append([])
+        for i in range(len(map)):
+            map[i] = lines[i].split(" ")
+        return Level(map)
 
 level = None  # Level class, overwritten when the loader is called
 
 while True:
+    load = Loader()
+    lvl = load.load_level(0)
+    lvl.draw_level()
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
