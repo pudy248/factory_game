1f99467dd0177d97659f2b599f29824c1de70824
pudy248 <lonnie.puddy@gmail.com>
2021-03-31 12:54:43 -0600

    tile subclasses, item class, tick method

--- main.py
+++ main.py
@@ -14,28 +14,63 @@ TILE_SIZE = 50  # dimensions of each tile in pixels
 #####################
 
 
+class Item:
+    def __init__(self, name):
+        self.name = name
+        self.image = None
+        self.direction = [0, 0]
+        self.moved = False
+
+
 class Tile:
     def __init__(self, pos):
         self.pos = pos
-        self.direction = [0, 0]
+        self.direction = [1, 0]
         self.image = pg.transform.scale(pg.image.load("pacman.png"), (TILE_SIZE, TILE_SIZE))
         self.resource = "None"  # None, Iron, Wood, Coal, Oil, Out of Bounds
         self.type = "Tile"
         self.items = []
 
     def draw(self):
-        SURF.blit(self.image, (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
+        SURF.blit(pg.transform.rotate(self.image, 180 if self.direction[0] == -1 else (180 - 90 * self.direction[1])), (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
 
     def tick(self):
-        pass  # empty for now, not sure what to put in the generic tile class since it is never used in-game
+        # Every tile has the ability to move items
+        for i in range(len(self.items)):
+            if self.items[i].moved:
+                continue
+            temp = self.items.pop(i)
+            i -= 1
+            temp.direction = self.direction
+            temp.moved = True
+            level.tile_array[self.pos[0] + self.direction[0]][self.pos[1] + self.direction[1]].items.append(temp)
 
     def is_open(self, type):
-        if self.resource is "Out of Bounds":
+        if self.resource == "Out of Bounds":
             return False
         elif self.resource is not None and type != "Extractor":
             return False
         return True
 
+
+class Extractor(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Extractor"
+
+    def tick(self):
+        # adds an item based on resources
+        super(Extractor, self).tick()
+
+
+class Manufacturer(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Manufacturer"
+
+    def tick(self):
+        # if Recipie Collection class says that the items in self.items can be made into a recipie, consumes them and outputs the result
+        super(Manufacturer, self).tick()
 
 
 class Player:
@@ -65,6 +100,8 @@ class Player:
         elif self.can_select():
             self.selected_tile = self.get_tile()
 
+
+level = None  # Level class, overwritten when the loader is called
 
 while True:
     for event in pg.event.get():
