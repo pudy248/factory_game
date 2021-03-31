e0de6ce220c978ed198adce5af223f0f7fa90f99
mybarlucea <78443586+mybarlucea@users.noreply.github.com>
2021-03-31 09:33:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -42,59 +42,6 @@ class Loader:
             map[i] = lines[i].split(" ")
         return Level(map)
 
-class Tile:
-    def __init__(self, pos):
-        self.pos = pos
-        self.direction = [0, 0]
-        self.image = pg.transform.scale(pg.image.load("pacman.png"), (TILE_SIZE, TILE_SIZE))
-        self.resource = "None"  # None, Iron, Wood, Coal, Oil, Out of Bounds
-        self.items = []
-
-    def draw(self):
-        SURF.blit(self.image, (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
-
-    def tick(self):
-        pass  # empty for now, not sure what to put in the generic tile class since it is never used in-game
-
-    def is_open(self, type):
-        if self.resource is "Out of Bounds":
-            return False
-        elif self.resource is not None and type != "Extractor":
-            return False
-        return True
-
-
-class Player:
-    def __init__(self):
-        self.selected_tile = False
-        self.last_pos = (0, 0)
-
-    def is_in_level(self):  # Detects if pos is within the level
-        return self.last_pos[1] < 300
-
-    def can_place(self):
-        return self.selected_tile and self.get_tile().is_open(self.selected_tile.type)
-
-    def can_select(self):
-        if self.get_tile:
-            return True
-        return False
-
-    def get_tile(self):
-        return level.tiles[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE]
-
-    def place(self):
-        level.tiles[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE] = self.selected_tile
-
-    def click(self, pos):
-        self.last_pos = pos
-        if self.is_in_level():
-            if self.can_place():
-                self.place()
-        elif self.can_select():
-            self.selected_tile = self.get_tile()
-
-
 while True:
     load = Loader()
     lvl = load.load_level(0) # 0.txt is just a dummy for testing
