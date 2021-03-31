238cf50340be91c51dad94f7c6882beae2b93269
htsvr <brrgrra@gmail.com>
2021-03-31 13:09:05 -0600

    Add player controls

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 SURF = pg.display.set_mode((W, H), pg.NOFRAME)
 
-
 #####CONSTANTS#####
 FPS = 60
 TILE_SIZE = 50  # dimensions of each tile in pixels
@@ -74,6 +73,7 @@ class Player:
         return lvl.map[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE]
 
     def place(self):  # works
+        self.selected_tile.pos = ((self.last_pos[0]//TILE_SIZE)*TILE_SIZE, (self.last_pos[1]//TILE_SIZE)*TILE_SIZE)
         lvl.map[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE] = self.selected_tile
 
     def click(self, pos):
@@ -81,8 +81,11 @@ class Player:
         if self.is_in_level():
             if self.can_place():
                 self.place()
-        elif self.can_select():
-            self.selected_tile = self.get_tile()
+
+    def select(self, key):
+        if key == pg.K_1:
+            print("selected")
+            self.selected_tile = Tile(self.last_pos)
 
 class Extractor(Tile):
     def __init__(self, pos):
@@ -136,10 +139,16 @@ load = Loader()
 lvl = load.load_level(0) # 0.txt is just a dummy for testing
 player = Player()
 while True:
-    lvl.draw_level() # only here to test that the file was parsed properly
+    # lvl.draw_level() # only here to test that the file was parsed properly
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
             sys.exit()
+        elif event.type == pg.KEYDOWN:
+            player.select(event.key)
+        elif event.type == pg.MOUSEBUTTONUP:
+            lvl.draw_level()
+            player.click(event.pos)
+            lvl.draw_level()
     pg.display.update()
     clock.tick(FPS)
