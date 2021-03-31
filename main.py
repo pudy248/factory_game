9efa0889d393be22892dcb9e4e38a88fd13a29ae
mybarlucea <21michael.ybarlucea@boiseschools.net>
2021-03-31 12:50:57 -0600

    implemented level drawing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ clock = pg.time.Clock()
 W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 SURF = pg.display.set_mode((W, H), pg.NOFRAME)
-
+img = pg.image.load('pacman.png')
 
 #####CONSTANTS#####
 FPS = 60
@@ -15,14 +15,18 @@ TILE_SIZE = 50  # dimensions of each tile in pixels
 #####################
 
 class Level:
-    def __init__(self, map):
-        self.map = map
+    def __init__(self, tMap):
+        self.map = tMap
 
     def world_tick(self):
         x = 4  # placeholder
 
     def draw_level(self):
-        print(self.map)  # just for testing
+        side = len(self.map)
+        for tiley in range(side):
+            for tilex in range(side):
+                if self.map[tiley][tilex] == "1":
+                    SURF.blit(img, ((side-tilex - 1) * TILE_SIZE, tiley * TILE_SIZE))
 
 class Loader:
     def __init__(self):
@@ -45,7 +49,7 @@ class Loader:
 while True:
     load = Loader()
     lvl = load.load_level(0)  # 0.txt is just a dummy for testing
-    lvl.draw_level()  # only here to test that the file was parsed properly
+    lvl.draw_level()
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
