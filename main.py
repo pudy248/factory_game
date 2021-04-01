8dd1b14f7b7c032e622cb052d477251f92da1ba3
mybarlucea <21michael.ybarlucea@boiseschools.net>
2021-04-01 12:55:10 -0600

    added changes from master

--- main.py
+++ main.py
@@ -7,30 +7,45 @@ clock = pg.time.Clock()
 W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 SURF = pg.display.set_mode((W, H), pg.NOFRAME)
-img = pg.image.load('pacman.png')
 
 #####CONSTANTS#####
 FPS = 60
 TILE_SIZE = 50  # dimensions of each tile in pixels
 #####################
 
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+        self.image = None
+        self.direction = [0, 0]
+        self.moved = False
+
+
 class Tile:
     def __init__(self, pos):
         self.pos = pos
-        self.direction = [0, 0]
+        self.direction = [1, 0]
         self.image = pg.transform.scale(pg.image.load("pacman.png"), (TILE_SIZE, TILE_SIZE))
         self.resource = "None"  # None, Iron, Wood, Coal, Oil, Out of Bounds
-        self.type = "Tile"
         self.items = []
 
     def draw(self):
-        SURF.blit(self.image, (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
+        SURF.blit(pg.transform.rotate(self.image, 180 if self.direction[0] == -1 else (180 - 90 * self.direction[1])), (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
 
     def tick(self):
-        pass  # empty for now, not sure what to put in the generic tile class since it is never used in-game
+        # Every tile has the ability to move items
+        for i in range(len(self.items)):
+            if self.items[i].moved:
+                continue
+            temp = self.items.pop(i)
+            i -= 1
+            temp.direction = self.direction
+            temp.moved = True
+            level.tile_array[self.pos[0] + self.direction[0]][self.pos[1] + self.direction[1]].items.append(temp)
 
     def is_open(self, type):
-        if self.resource is "Out of Bounds":
+        if self.resource == "Out of Bounds":
             return False
         elif self.resource is not None and type != "Extractor":
             return False
@@ -52,6 +67,7 @@ class Level:
 class Loader:
     def __init__(self):
         self.lNum = 0
+
 
     def load_level(self, n):
         self.lNum = n
@@ -80,10 +96,29 @@ class Loader:
                 newMap[y][x] = Tile(pos)
         return Level(newMap)
 
+class Extractor(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Extractor"
+
+    def tick(self):
+        # adds an item based on resources
+        super(Extractor, self).tick()
+
+
+class Manufacturer(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Manufacturer"
+
+    def tick(self):
+        # if Recipie Collection class says that the items in self.items can be made into a recipie, consumes them and outputs the result
+        super(Manufacturer, self).tick()
+
+
+level = None  # Level class, overwritten when the loader is called
+
 while True:
-    load = Loader()
-    lvl = load.load_level(0)  # 0.txt is just a dummy for testing
-    lvl.draw_level()
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
