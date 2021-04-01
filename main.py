33592b833fdc95cb9d7ace97c77b820fba14f9e0
pudy248 <lonnie.puddy@gmail.com>
2021-04-01 13:04:49 -0600

    splitter, intersection, belt also changed direction to a pygame vector

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ SURF = pg.display.set_mode((W, H), pg.NOFRAME)
 
 #####CONSTANTS#####
 FPS = 60
-TILE_SIZE = 50  # dimensions of each tile in pixels
+TILE_SIZE = 100  # dimensions of each tile in pixels
 #####################
 
 
 class Item:
     def __init__(self, name):
         self.name = name
-        self.image = None
-        self.direction = [0, 0]
+        self.image = pg.image.load("New Piskel-1.png.png")
+        self.direction = pg.Vector2([0, 0])
         self.moved = False
 
 
 class Tile:
     def __init__(self, pos):
         self.pos = pos
-        self.direction = [1, 0]
+        self.direction = pg.Vector2([0, 1])
         self.image = pg.transform.scale(pg.image.load("pacman.png"), (TILE_SIZE, TILE_SIZE))
         self.resource = "None"  # None, Iron, Wood, Coal, Oil, Out of Bounds
         self.type = "Tile"
         self.items = []
 
     def draw(self):
-        SURF.blit(pg.transform.rotate(self.image, 180 if self.direction[0] == -1 else (180 - 90 * self.direction[1])), (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
+        SURF.blit(pg.transform.rotate(self.image, -self.direction.angle_to(pg.Vector2([1, 0]))), (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
 
     def tick(self):
         # Every tile has the ability to move items
@@ -43,7 +43,7 @@ class Tile:
             i -= 1
             temp.direction = self.direction
             temp.moved = True
-            level.tile_array[self.pos[0] + self.direction[0]][self.pos[1] + self.direction[1]].items.append(temp)
+            level.tile_array[self.pos[0] + self.direction.x][self.pos[1] + self.direction.y].items.append(temp)
 
     def is_open(self, type):
         if self.resource == "Out of Bounds":
@@ -61,6 +61,7 @@ class Extractor(Tile):
     def tick(self):
         # adds an item based on resources
         super(Extractor, self).tick()
+        # TODO implement this
 
 
 class Manufacturer(Tile):
@@ -71,11 +72,68 @@ class Manufacturer(Tile):
     def tick(self):
         # if Recipie Collection class says that the items in self.items can be made into a recipie, consumes them and outputs the result
         super(Manufacturer, self).tick()
+        # TODO implement this
+
+
+class Belt(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Belt"
+
+    def draw(self):
+        super(Belt, self).draw()
+        if len(self.items) > 0:
+            img = pg.transform.scale(self.items[0].image, (int(TILE_SIZE / 2), int(TILE_SIZE / 2)))
+            SURF.blit(img, (self.pos[0] * TILE_SIZE + TILE_SIZE / 4, self.pos[1] * TILE_SIZE + TILE_SIZE / 4))
+
+
+class Intersection(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Intersection"
+
+    def tick(self):
+        # Respects item direction
+        for i in range(len(self.items)):
+            if self.items[i].moved:
+                continue
+            temp = self.items.pop(i)
+            i -= 1
+            temp.moved = True
+            level.tile_array[self.pos[0] + temp.direction.x][self.pos[1] + temp.direction.y].items.append(temp)
+
+
+class Splitter(Tile):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = "Splitter"
+        self.split_bool = False  # False = right, True = left
+
+    def tick(self):
+        # Alternates between left and right
+        for i in range(len(self.items)):
+            if self.items[i].moved:
+                continue
+            self.split_bool = not self.split_bool
+            temp = self.items.pop(i)
+            i -= 1
+            direction = pg.Vector2(self.direction[0], self.direction[1])
+            temp.direction = direction.rotate(90 if self.split_bool else 270)
+            temp.moved = True
+            level.tile_array[self.pos[0] + temp.direction.x][self.pos[1] + temp.direction.y].items.append(temp)
+
+
+
+
 
 
 level = None  # Level class, overwritten when the loader is called
 
 while True:
+    tile = Belt((1, 1))
+    tile.direction = pg.Vector2([0, 1])
+    tile.items.append(Item("Iron"))
+    tile.draw()
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
