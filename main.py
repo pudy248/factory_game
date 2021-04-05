da481d4089038ec14d7a12b556604b03fc36fe9f
htsvr <brrgrra@gmail.com>
2021-04-05 13:12:24 -0600

    tested player class

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Level:
         for tiley in range(self.side):
             for tilex in range(self.side):
                 self.tile_array[tiley][tilex].draw()
+                print(self.tile_array[tiley][tilex].__str__())
 
 
 class Loader:
@@ -83,6 +84,9 @@ class Tile:
         self.type = "Tile"
         self.items = []
 
+    def __str__(self):
+        return str(self.type) + ": " + str(self.pos)
+
     def draw(self):
         SURF.blit(pg.transform.rotate(self.image, -self.direction.angle_to(pg.Vector2([1, 0]))), (self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE))
 
@@ -113,20 +117,28 @@ class Tile:
 class Player:
     def __init__(self):
         self.selected_tile = False
-        self.last_pos = (0, 0)
+        self.last_pos = [0, 0]
 
     def is_in_level(self):  # Detects if pos is within the level
-        return self.last_pos[0] < TILE_SIZE*len(lvl.map[0]) and self.last_pos[1] < TILE_SIZE*len(lvl.map)
+        return self.last_pos[0] < TILE_SIZE * len(level.tile_array[0]) and self.last_pos[1] < TILE_SIZE * len(level.tile_array)
 
     def can_place(self):  # should work
-        return self.selected_tile and self.get_tile().is_open(self.selected_tile.type)
+        return self.selected_tile and self.get_tile().is_open(self.selected_tile)
 
     def get_tile(self):  # works
-        return lvl.map[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE]
+        return level.tile_array[self.last_pos[1] // TILE_SIZE][self.last_pos[0] // TILE_SIZE]
 
     def place(self):  # works
-        self.selected_tile.pos = ((self.last_pos[0]//TILE_SIZE)*TILE_SIZE, (self.last_pos[1]//TILE_SIZE)*TILE_SIZE)
-        lvl.map[self.last_pos[1]//TILE_SIZE][self.last_pos[0]//TILE_SIZE] = self.selected_tile
+        print((self.last_pos[0]//TILE_SIZE)*TILE_SIZE)
+        print((self.last_pos[1]//TILE_SIZE)*TILE_SIZE)
+        #self.selected_tile.pos = [(self.last_pos[0]//TILE_SIZE), (self.last_pos[1]//TILE_SIZE)]
+        print(str(self.last_pos[1]//TILE_SIZE) + ", " + str(self.last_pos[0]//TILE_SIZE))
+        for i in level.tile_array:
+            for j in i:
+                print(j.__str__())
+        print(level.tile_array)
+        if self.selected_tile == "Extractor":
+            level.tile_array[self.last_pos[1] // TILE_SIZE][self.last_pos[0] // TILE_SIZE] = Extractor([(self.last_pos[0]//TILE_SIZE), (self.last_pos[1]//TILE_SIZE)], 0)
 
     def click(self, pos):
         self.last_pos = pos
@@ -136,16 +148,17 @@ class Player:
 
     def select(self, key):
         if key == pg.K_1:
-            self.selected_tile = Extractor(self.last_pos)
+            self.selected_tile = "Extractor"
         elif key == pg.K_2:
-            self.selected_tile = Manufacturer(self.last_pos)
+            self.selected_tile = Manufacturer(self.last_pos, 0)
         elif key == pg.K_3:
-            self.selected_tile = Tile(self.last_pos)
+            self.selected_tile = Tile(self.last_pos, 0)
 
 class Extractor(Tile):
     def __init__(self, pos, angle):
         super().__init__(pos, angle)
         self.type = "Extractor"
+        self.image = pg.transform.scale(pg.image.load("sprites\\tile_grass.png"), (TILE_SIZE, TILE_SIZE))
 
     def tick(self):
         # adds an item based on resources
@@ -213,10 +226,10 @@ class Splitter(Tile):
 level = None  # Level class, overwritten when the loader is called
 
 load = Loader()
-lvl = load.load_level(0) # 0.txt is just a dummy for testing
+level = load.load_level(0) # 0.txt is just a dummy for testing
 player = Player()
 while True:
-    # lvl.draw_level() # only here to test that the file was parsed properly
+    level.draw_level() # only here to test that the file was parsed properly
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             pg.quit()
